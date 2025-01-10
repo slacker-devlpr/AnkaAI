f5cc2bed62f3f5cea251bf41c2654e8410479162
slacker-devlpr <shubstay@gmail.com>
2025-01-10 16:17:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -109,6 +109,7 @@ if prompt := st.chat_input("How can I help?"):
             "Make sure that only the mathematical formulas are surrounded by $$ and that you don't surround other text with it. "
             "Make sure to put the correct latex code around the math equation."
             "Do not use ( in anything from now on if you want to display anything latex(math for example) you surrond the latex with $$"
+            "Use **examlple** or **example** to highlight titles/subtitles. Do not use your way of listing items. Never list anything write it another way and use titles/subtitles."
         )
     }
 
