108699eefd71bdb433d9b3c63022a7c2a57bf2e7
slacker-devlpr <shubstay@gmail.com>
2025-01-10 16:26:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,9 +36,6 @@ USER_AVATAR = "👤"
 BOT_AVATAR = r"Anka (1).png"
 client = OpenAI(api_key='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')
 
-with shelve.open("chat_history") as db:
-    if "messages" in db:
-        del db["messages"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o-mini"
@@ -71,6 +68,11 @@ def render_latex(text):
         else:
             st.markdown(part)
 
+def display_messages(messages):
+    for message in messages:
+      avatar = USER_AVATAR if message["role"] == "user" else BOT_AVATAR
+      with st.chat_message(message["role"], avatar=avatar):
+          render_latex(message["content"])
 
 # Load chat history if not already in session
 if "messages" not in st.session_state:
@@ -86,18 +88,16 @@ if not st.session_state.messages:
     st.toast("You are currently running Anka-AI 1.0.4.", icon="⚙️")
     st.session_state.messages.append(initial_message)
 
-# Display chat messages
-for message in st.session_state.messages:
-    avatar = USER_AVATAR if message["role"] == "user" else BOT_AVATAR
-    with st.chat_message(message["role"], avatar=avatar):
-        render_latex(message["content"]) 
 
+# Display chat messages
+display_messages(st.session_state.messages)
+    
 
 # Main chat interface
 if prompt := st.chat_input("How can I help?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user", avatar=USER_AVATAR):
-       st.markdown(prompt)
+        st.markdown(prompt)
 
     system_message = {
         "role": "system",
@@ -121,5 +121,6 @@ if prompt := st.chat_input("How can I help?"):
     print(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
     with st.chat_message("assistant", avatar=BOT_AVATAR):
-         type_response(response)
+        type_response(response)
+    save_chat_history(st.session_state.messages)
 
