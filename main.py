f3ae2308b104eb14cfb74c8e52f03c131dd96b78
slacker-devlpr <shubstay@gmail.com>
2025-01-28 20:42:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(
     page_title="Anka-AI, artificial intelligence for math",
-    page_icon=r"Anka (1).png"
+    page_icon=r"Anka (4).png"
 )
 hide_streamlit_style = """
 <style>
