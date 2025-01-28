c9524c179ebc1fcdb82803d60bdd92f142c8091f
slacker-devlpr <shubstay@gmail.com>
2025-01-28 15:47:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
+import streamlit as st
 st.title("Dear Valued Users,")
 st.text("We regret to inform you that Anka AI will be shutting down as we transition to DeepSeek-V3 which achieves a significant breakthrough in inference speed over previous models. While this marks the end of an era, it is also the beginning of a fresh chapter with enhanced capabilities and features to better serve your needs.")
