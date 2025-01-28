966fd168bca426c90e2887f8e889480452b406f2
slacker-devlpr <shubstay@gmail.com>
2025-01-28 19:25:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,38 @@ st.set_page_config(
     page_title="Anka-AI, artificial intelligence for math",
     page_icon=r"Anka (1).png"
 )
-
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 st.title("Dear Valued Users,")
 st.text("We regret to inform you that Anka AI will be shutting down as we transition from OpenAI to Deepseek which achieves a significant breakthrough in inference speed over previous models.")
 st.text("While this marks the end of an era, it is also the beginning of a fresh chapter with enhanced capabilities and features to better serve your needs. ")
