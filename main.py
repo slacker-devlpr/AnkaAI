3214932324f2f0931480cb8de62497f156f16080
slacker-devlpr <shubstay@gmail.com>
2025-01-28 15:56:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title("Dear Valued Users,")
-st.text("We regret to inform you that Anka AI will be shutting down as we transition from *OpenAI* to *Deepseek* which achieves a significant breakthrough in inference speed over previous models. /n While this marks the end of an era, it is also the beginning of a fresh chapter with enhanced capabilities and features to better serve your needs. /n We deeply appreciate your support and understanding during this change. This decision was made with the goal of providing you with an even more innovative and robust AI experience. /n Thank you for being part of this journey, and we look forward to continuing to support you with our new platform.")
+st.text("We regret to inform you that Anka AI will be shutting down as we transition from OpenAI to Deepseek which achieves a significant breakthrough in inference speed over previous models.")
+st.text("While this marks the end of an era, it is also the beginning of a fresh chapter with enhanced capabilities and features to better serve your needs. ")
+st.text("We deeply appreciate your support and understanding during this change. This decision was made with the goal of providing you with an even more innovative and robust AI experience. ")
+st.text("Thank you for being part of this journey, and we look forward to continuing to support you with our new platform.")
